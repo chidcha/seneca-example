449179dfc6d702210f619eb24d364d13446ccdec
chidcha <cchoksuchat@hotmail.com>
2019-02-09 12:39:11 +0700

    Update index.js

--- hello/index.js
+++ hello/index.js
@@ -1,7 +1,7 @@
 const seneca = require('seneca')();
 
 let hello = (msg, reply) => {
-    reply(null, {answer: ('Hello ' + msg.name)})
+    reply(null, {answer: ('Hello ' + msg.name + ', Please listen to me!' )})
 };
 
 seneca.add('service:hello', hello).listen();
